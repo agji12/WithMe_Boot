d082432d797f0b3d0845f998601fef3814de18bb
agji12 <agji12@naver.com>
2023-11-16 20:32:43 +0900

    first commit

---
+++ WithMe/src/main/java/com/project/wm/WithMeApplication.java
@@ -0,0 +1,13 @@
+package com.project.wm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WithMeApplication.class, args);
+	}
+
+}
