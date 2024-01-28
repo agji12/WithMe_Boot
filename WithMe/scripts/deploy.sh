e0daa638a803e2eb24059a85edce4ecdba1e03f2
agji12 <127912681+agji12@users.noreply.github.com>
2024-01-28 21:28:27 +0900

    Update deploy.sh

--- WithMe/scripts/deploy.sh
+++ WithMe/scripts/deploy.sh
@@ -1,17 +1,31 @@
-PROJECT_ROOT="/home/ubuntu/app/WithMe/target"
-JAR_FILE="$PROJECT_ROOT/WithMe-0.0.1-SNAPSHOT.jar"
+#!/bin/bash
+
+REPOSITORY=/home/ubuntu/app/WithMe
+cd $REPOSITORY
+
+APP_NAME=demo
+# JAR_NAME=$(ls $REPOSITORY/target/ | grep '.jar' | tail -n 1)
+JAR_PATH=$REPOSITORY/target/WithMe-0.0.1-SNAPSHOT.jar
+
+CURRENT_PID=$(pgrep -fl java)
+
+if [ -z "$CURRENT_PID" ]; then
+    echo "NOT RUNNING"
+else
+    echo "> kill -9 $CURRENT_PID"
+    kill -15 $CURRENT_PID
+    sleep 5
+fi
 
 APP_LOG="$PROJECT_ROOT/application.log"
 ERROR_LOG="$PROJECT_ROOT/error.log"
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
-TIME_NOW=$(date +%c)
+echo "> $JAR_PATH 에 실행권한 추가"
+chmod +x $JAR_PATH
 
-# echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
-# cp $PROJECT_ROOT/*.jar $JAR_FILE
+echo "> $JAR_PATH 배포"
+nohup java -jar $JAR_PATH > $APP_LOG 2> $ERROR_LOG &
 
-echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
-nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
-
-CURRENT_PID=$(pgrep -f $JAR_FILE)
-echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
+CURRENT_PID=$(pgrep -f $JAR_PATH)
+echo " > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
